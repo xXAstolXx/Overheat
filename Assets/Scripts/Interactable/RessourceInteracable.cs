98329084bf170a00ccf45644d1b7c45496aedf39
David Teinert <DavidTeinert344@t-online.de>
2023-12-26 19:11:23 +0100

    changes in RessourceInteractable removed some unused shit

--- Assets/Scripts/Interactable/RessourceInteracable.cs
+++ Assets/Scripts/Interactable/RessourceInteracable.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RessourceInteracable : MonoBehaviour
@@ -22,14 +19,6 @@ public class RessourceInteracable : MonoBehaviour
         {
             collector.TryCollect(this, data);
         }
-    }
-
-    private void Interact(GameObject target)
-    {
-        gameObject.transform.localPosition = new Vector3(0,0,0); 
-        transform.SetParent(target.GetComponentInChildren<BackPack>().gameObject.transform, false);
-        target.gameObject.GetComponentInChildren<BackPack>().ressourceDatas.Add(data);
-        gameObject.transform.localScale = new Vector3(0.5f,0.5f,1);
-        Debug.Log("!!!inetracted!!!");
+        return;
     }
 }
