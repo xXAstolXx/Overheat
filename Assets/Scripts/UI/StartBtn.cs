4f68125a7cb31eddbafdbef5570642a08284fd3f
David Teinert <DavidTeinert344@t-online.de>
2024-02-21 22:23:42 +0100

    saves

--- Assets/Scripts/UI/StartBtn.cs
+++ Assets/Scripts/UI/StartBtn.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartBtn : MonoBehaviour
 {
     [SerializeField]
     private int sceneIndex;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField]
+    private Slider slider;
+
+    private void Start()
     {
-        Debug.Log("trigger start");
-        SceneManager.LoadScene(sceneIndex);
+        slider.maxValue = 10;
+        slider.minValue = 0;
+        slider.value = slider.minValue;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        slider.value += 0.1f;
+        if(slider.value == slider.maxValue)
+        {
+            LoadSceneByIndex(sceneIndex);
+        }
+    }
+
+    private void LoadSceneByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
     }
 }
